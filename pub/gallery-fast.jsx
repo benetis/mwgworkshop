9edb16d13bb4fe34d9c897b635124809baa65cc5
Zygimantas Benetis <zygimantasdev@gmail.com>
2016-12-03 13:34:01 +0200

    image loading on what we need works

--- pub/gallery-fast.jsx
+++ pub/gallery-fast.jsx
@@ -1,10 +1,30 @@
 const GalleryView = React.createClass({
   render() {
+    const topSpace = {
+      height: this.props.topSpace
+    };
+
+    const bottomSpace = {
+      height: this.props.bottomSpace
+    };
+
+    const imageSize = {
+      height: this.props.imageSize +'px'
+    };
+
     return (
       <div id="gallery"
            className="fast">
+        <div style={topSpace}></div>
+
         {_.map(this.props.images, image =>
-          <img src={image.low_resolution.url}/>)}
+          <img style={imageSize}
+               src={image.low_resolution.url}
+          />
+        )}
+
+        <div style={bottomSpace}></div>
+
       </div>
     )
   }
@@ -12,9 +32,14 @@ const GalleryView = React.createClass({
 
 const galleryComp = React.createClass({
 
+  imageSize: 320,
+  goodQualityBuffer: 10,
+
   getInitialState: function () {
     return {
-      imagesToLoad: this.takeFiveClosest()
+      imagesToLoad: this.getGoodQualityPhotos(),
+      topSpace: 0,
+      bottomSpace: this.bottomSpaceCounter(0)
     };
   },
 
@@ -29,17 +54,35 @@ const galleryComp = React.createClass({
 
   handleScroll() {
     this.setState({
-      imagesToLoad: this.takeFiveClosest()
+      imagesToLoad: this.getGoodQualityPhotos()
     })
   },
 
-  takeFiveClosest() {
-    return this.props.images.slice(0, 5);
+  getGoodQualityPhotos() {
+    this.setState({
+      topSpace: this.imagesScrolledPassed() * this.imageSize,
+      bottomSpace: this.bottomSpaceCounter()
+    });
+
+    return this.props.images.slice(this.imagesScrolledPassed(), this.imagesScrolledPassed() + this.goodQualityBuffer);
+  },
+
+  bottomSpaceCounter(scrolledFromTop) {
+    return (this.props.images.length * this.imageSize) - scrolledFromTop;
+  },
+
+  imagesScrolledPassed() {
+    const scrollTop = window.pageYOffset;
+    return Math.floor(scrollTop / this.imageSize);
   },
 
   render() {
     return (
-      <GalleryView images={this.state.imagesToLoad}/>
+      <GalleryView images={this.state.imagesToLoad}
+                   topSpace={this.state.topSpace}
+                   bottomSpace={this.state.bottomSpace}
+                   imageSize={this.imageSize}
+      />
     )
   }
 });
