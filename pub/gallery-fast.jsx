8a2f622ffc4d6f429f50e365f77f21c74f54c73d
Zygimantas Benetis <zygimantasdev@gmail.com>
2016-12-03 13:02:59 +0200

    takes first five images

--- pub/gallery-fast.jsx
+++ pub/gallery-fast.jsx
@@ -1,7 +1,46 @@
-const galleryComp = React.createClass({
-    render() {
-        return <div id="gallery" className="fast">
-            {_.map(this.props.images, image => <img src={image.low_resolution.url}/>)}
-        </div>
-    }
+const GalleryView = React.createClass({
+  render() {
+    return (
+      <div id="gallery"
+           className="fast">
+        {_.map(this.props.images, image =>
+          <img src={image.low_resolution.url}/>)}
+      </div>
+    )
+  }
 });
+
+const galleryComp = React.createClass({
+
+  getInitialState: function () {
+    return {
+      imagesToLoad: this.takeFiveClosest()
+    };
+  },
+
+  //todo resize event
+  componentDidMount: function () {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+
+  componentWillUnmount: function () {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+
+  handleScroll() {
+    this.setState({
+      imagesToLoad: this.takeFiveClosest()
+    })
+  },
+
+  takeFiveClosest() {
+    return this.props.images.slice(0, 5);
+  },
+
+  render() {
+    return (
+      <GalleryView images={this.state.imagesToLoad}/>
+    )
+  }
+});
+
